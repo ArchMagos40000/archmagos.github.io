bb29ab55de5cea4ebdff0fd84ad1dab75c2f174d
ArchMagos40000 <113367046+ArchMagos40000@users.noreply.github.com>
2025-07-13 22:11:55 -0400

    Add files via upload

---
+++ Gradebook.js
@@ -0,0 +1,20 @@
+// Stub: Load grade data from the database
+function loadGrades() {
+  // This function will load grades from the database
+  console.log("loadGrades() called");
+}
+
+// Stub: Add a new student row to the gradebook
+function addStudentRow(studentName) {
+  // This function will add a student to the gradebook table
+  console.log("addStudentRow() called for", studentName);
+}
+
+// Stub: Populate a grade cell
+function addGrade(studentName, assignmentIndex, grade) {
+  // This function will populate the table with a student's grade
+  console.log(`addGrade() called for ${studentName}, assignment ${assignmentIndex}, grade ${grade}`);
+}
+
+// Call the stub to simulate behavior
+loadGrades();
